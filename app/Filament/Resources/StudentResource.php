7cbd03e6720b4e13bfed2b59c2f20ab01f25e7ca
ahmadkureshi <mudassirsarfraz@gmail.com>
2023-09-06 11:28:42 +0500

    Relation Manager Filament

--- app/Filament/Resources/StudentResource.php
+++ app/Filament/Resources/StudentResource.php
@@ -71,7 +71,7 @@ class StudentResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            RelationManagers\GuardiansRelationManager::class
         ];
     }
 
